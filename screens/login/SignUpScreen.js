4060764d76787fbb40a3ec0fba6e1f43e8d81cb4
Charlene Hoareau <charlene.hoareau@outlook.com>
2023-07-31 19:37:04 +0200

    style signupscreen

--- screens/login/SignUpScreen.js
+++ screens/login/SignUpScreen.js
@@ -1,4 +1,5 @@
 import { Platform, SafeAreaView, StatusBar, TouchableOpacity, KeyboardAvoidingView, StyleSheet, Text, View, TextInput } from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 export default function SignUpScreen({ navigation }) {
   return (
@@ -13,77 +14,104 @@ export default function SignUpScreen({ navigation }) {
         </View>
 
         <View style={styles.connect}>
-        <TouchableOpacity onPress={() => navigation.navigate("TabNavigator")} style={styles.buttonHalfGreen} >
-          <Text style={styles.textButtonHalfPlain}>Facebook</Text>
+        <TouchableOpacity onPress={() => navigation.navigate("TabNavigator")} style={styles.buttonFB} >
+          <FontAwesome name='facebook' size={20} color='#ffffff' />
+          <Text style={styles.textButtonFB}>Facebook</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate("TabNavigator")} style={styles.buttonHalfWhite} >
-          <Text style={styles.textButtonHalfWhite}>Google</Text>
+        <TouchableOpacity onPress={() => navigation.navigate("TabNavigator")} style={styles.buttonG} >
+          <FontAwesome name='google' size={20} color='#ffffff' />
+          <Text style={styles.textButtonG}>Google</Text>
         </TouchableOpacity>
         </View>
 
         <View style={styles.bar}></View>
 
-        <View  style={styles.inputContainer}>
+        <View style={styles.title3Container}>
         <Text style={styles.titleh3}>ou</Text>
+        </View>
 
-        <TextInput
-            placeholder="Firstname"
+        <View  style={styles.inputContainer}>
+        <View style={styles.input}>
+        <FontAwesome name='user' size={20} color='#353639' style={styles.icon} />
+          <TextInput
+            placeholder="Prénom"
             autoCapitalize="none" // https://reactnative.dev/docs/textinput#autocapitalize
             // keyboardType="email-address" //https://reactnative.dev/docs/textinput#keyboardtype pour les emails/numbers
             textContentType="name" // https://reactnative.dev/docs/textinput#textcontenttype-ios
             autoComplete="name" // https://reactnative.dev/docs/textinput#autocomplete-android
             // onChangeText={(value) => setEmail(value)}
             // value={email}
-            style={styles.input}
-            placeholderTextColor='#198EA5'
-          />
-        <TextInput
-            placeholder="Lastname"
-            autoCapitalize="none" 
-            textContentType="name" 
-            autoComplete="family-name" 
-            // onChangeText={(value) => setEmail(value)}
-            // value={email}
-            style={styles.input}
-            placeholderTextColor='#198EA5'
-          />
-          <TextInput
-            placeholder="Email"
-            autoCapitalize="none"
-            keyboardType="email-address"
-            textContentType="emailAddress"
-            autoComplete="email"
-            // onChangeText={(value) => setEmail(value)}
-            // value={email}
-            style={styles.input}
-            placeholderTextColor='#198EA5'
-          />
-
+            // style={styles.input}
+            placeholderTextColor='#353639'
+            />
+          </View>
+          <View style={styles.input}>
+            <FontAwesome name='user' size={20} color='#353639' style={styles.icon} />
+            <TextInput
+              placeholder="Nom"
+              autoCapitalize="none" 
+              textContentType="name" 
+              autoComplete="family-name" 
+              // onChangeText={(value) => setEmail(value)}
+              // value={email}
+              placeholderTextColor='#353639'
+            />
+            </View>
+            <View style={styles.input}>
+            <FontAwesome name='user' size={20} color='#353639' style={styles.icon} />
+            <TextInput
+              placeholder="Pseudo"
+              autoCapitalize="none" 
+              textContentType="name" 
+              autoComplete="family-name" 
+              // onChangeText={(value) => setEmail(value)}
+              // value={email}
+              placeholderTextColor='#353639'
+            />
+            </View>
+            <View style={styles.input}>
+              <FontAwesome name='at' size={20} color='#353639' style={styles.icon} />
+              <TextInput
+                placeholder="Email"
+                autoCapitalize="none"
+                keyboardType="email-address"
+                textContentType="emailAddress"
+                autoComplete="email"
+                // onChangeText={(value) => setEmail(value)}
+                // value={email}
+                placeholderTextColor='#353639'
+              />
+            </View>
           {/* {emailError && <Text style={styles.error}>Invalid email address</Text>} */}
-        <TextInput
-            placeholder="Password"
-            autoCapitalize="none"
-            textContentType="password" 
-            autoComplete="new-password" 
-            // onChangeText={(value) => setEmail(value)}
-            // value={email}
-            style={styles.input}
-            placeholderTextColor='#198EA5'
-          />
-          <TextInput
-            placeholder="Phone"
-            autoCapitalize="none" 
-            textContentType="telephoneNumber" 
-            autoComplete="tel" 
-            // onChangeText={(value) => setEmail(value)}
-            // value={email}
-            style={styles.input}
-            placeholderTextColor='#198EA5'
-          />
+          <View style={styles.input}>
+              <FontAwesome name='lock' size={20} color='#353639' style={styles.icon} />
+              <TextInput
+                  placeholder="Mot de passe"
+                  autoCapitalize="none"
+                  textContentType="password" 
+                  autoComplete="new-password" 
+                  // onChangeText={(value) => setEmail(value)}
+                  // value={email}
+                  placeholderTextColor='#353639'
+                />
+          </View>
+          <View style={styles.input}>
+              <FontAwesome name='phone' size={20} color='#353639' style={styles.icon} />
+              <TextInput
+                placeholder="Téléphone"
+                autoCapitalize="none" 
+                textContentType="telephoneNumber" 
+                autoComplete="tel" 
+                // onChangeText={(value) => setEmail(value)}
+                // value={email}
+                placeholderTextColor='#353639'
+              />
+          </View>
           </View>
 
           <View  style={styles.buttonContainer}>
         <TouchableOpacity onPress={() => navigation.navigate("TabNavigator")} style={styles.button} >
+          <FontAwesome name='hand-o-right' size={20} color='#ffffff' style={styles.icon} />
           <Text style={styles.textButton}>Valider</Text>
         </TouchableOpacity>
 
@@ -118,34 +146,37 @@ const styles = StyleSheet.create({
     fontSize: 25,
     fontWeight: '600',
     fontFamily: 'Futura',
-    marginBottom: 10
   },
   titleh3: {
     fontSize: 20,
     fontWeight: '600',
     fontFamily: 'Futura',
-    marginBottom: 10
+    // marginBottom: 40
   },
   connect: {
+    height: '5%',
     flexDirection: 'row',
-    width: '100%',
-    marginBottom: '5%'
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    // backgroundColor: 'red',
+    marginBottom: '10%'
   },
   button: {
     alignItems: 'center',
-    justifyContent: 'center',
     paddingTop: 8,
     marginTop: 30,
     backgroundColor: '#198EA5',
     borderRadius: 1,
     fontFamily: 'Futura',
-    width: '70%',
+    width: '80%',
+    height: '25%',
     borderRadius: 10,
+    flexDirection: 'row',
+    justifyContent: 'center'
   },
   buttonContainer: {
     alignItems: 'center',
     justifyContent: 'space-between',
-    marginTop: '10%',
     // backgroundColor: 'cyan'
   },
   textButton: {
@@ -157,21 +188,26 @@ const styles = StyleSheet.create({
   },
   input: {
     borderColor: '#198EA5',
-    borderWidth: 1,
-    fontSize: 20,
-    fontFamily: 'Futura',
-    width: '80%',
-    height: '12%',
+    borderWidth: 2,
+    fontSize: 16,
+    // fontFamily: 'Futura',
+    width: '94%',
+    height: '16%',
     borderRadius: 10,
     marginBottom: '5%',
-    paddingLeft: '5%'
+    paddingLeft: '5%',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginBottom: '15%'
   },
   inputContainer: {
     alignItems: 'center',
-    justifyContent: 'space-between',
+    justifyContent: 'space-evenly',
+    marginTop: '5%',
     // backgroundColor: "grey",
     padding: 30,
-    height: '50%'
+    height: '45%'
   },
   bar: {
     borderBottomColor : '#198EA5',
@@ -181,40 +217,49 @@ const styles = StyleSheet.create({
     marginLeft: 'auto',
     marginRight: 'auto',
   },
-  textButtonHalfPlain : {
+  textButtonFB : {
     fontFamily: 'Futura',
-    height: 30,
+    height: 35,
     fontWeight: '600',
     fontSize: 16,
     color: '#fff',
   },
-  textButtonHalfWhite : {
+  textButtonG: {
     fontFamily: 'Futura',
-    height: 30,
+    height: 35,
     fontWeight: '600',
     fontSize: 16,
     color: 'white',
   },
-  buttonHalfGreen: {
-    alignItems: 'center',
-    justifyContent: 'center',
+  buttonFB: {
     paddingTop: 8,
     marginTop: 30,
     backgroundColor: '#3B5998',
     borderRadius: 1,
     fontFamily: 'Futura',
-    width: '30%',
+    width: '35%',
     borderRadius: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly'
   },
-  buttonHalfWhite: {
-    alignItems: 'center',
-    justifyContent: 'center',
+  buttonG: {
     backgroundColor: '#DE4B39',
     paddingTop: 8,
     marginTop: 30,
     borderRadius: 1,
     fontFamily: 'Futura',
-    width: '30%',
+    width: '35%',
     borderRadius: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly'
   },
+  icon : {
+    marginRight: '3%',
+    marginBottom: '3%'
+  },
+  title3Container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '5%'
+  }
 });
