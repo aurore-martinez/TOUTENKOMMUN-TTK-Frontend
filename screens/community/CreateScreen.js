345be0b5e7f33bc832c5a2808b81ff0f62960573
Aurore Martinez <aurore.jp.martinez@gmail.com>
2023-07-31 19:05:18 +0200

    createscreen et joinscreen text sans style

--- screens/community/CreateScreen.js
+++ screens/community/CreateScreen.js
@@ -1,5 +1,7 @@
 import { Platform, SafeAreaView, StatusBar, TouchableOpacity } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 export default function CreateScreen({ navigation }) {
   return (
@@ -7,10 +9,38 @@ export default function CreateScreen({ navigation }) {
       <View>
         <Text>Screen Create!</Text>
 
+        <Text>Toutenkommun</Text>
+        <Text>Créer ma communauté</Text>
+        <TouchableOpacity
+          activeOpacity={0.8}
+        >
+          <View style={styles.icontext}>
+            <FontAwesome name="user" size='20' color='black' />
+            <Text>Nom de la communauté</Text>
+          </View>
+        </TouchableOpacity> 
+        <Text>Nom de communauté libre</Text>
+        <View style={styles.directionrow}>
+          <TouchableOpacity
+            activeOpacity={0.8}
+          >
+            <View style={styles.icontext}>
+              <FontAwesome name="lock" size='20' color='black' />
+              <Text>Privé</Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            activeOpacity={0.8}
+          >
+            <View style={styles.icontext}>
+              <FontAwesome5 name="door-open" size='15' color='black' />
+              <Text>Public</Text>
+            </View>
+          </TouchableOpacity> 
+        </View>      
         <TouchableOpacity onPress={() => navigation.navigate("Prêt")}>
           <Text>Créer</Text>
         </TouchableOpacity>
-
         <StatusBar style="auto" />
       </View>
     </SafeAreaView>
@@ -24,5 +54,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  },
+  icontext: {
+    flexDirection: 'row',
+  },
+  directionrow: {
+    flexDirection: 'row',
   },
 });
